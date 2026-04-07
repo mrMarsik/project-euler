0fd43c61f280e7057d8af36e0c5e3725911906b3
mrMarsik <petroimoroz@gmail.com>
2026-04-07 03:28:03 +0200

    fix-39

--- 001-050/044!!/index.js
+++ 001-050/044!!/index.js
@@ -7,17 +7,18 @@ for (let i = 1; i < 10000; i++) {
   arr.push(createPentagonNum(i))
 }
 
+// console.log(createPentagonNum(2000))
 let result = Infinity
 
 for (numK of arr) {
   for (numJ of arr) {
     
     if (arr.includes(numK + numJ)) {
-
-      if (arr.includes(Math.max(numK, numJ) - Math.min(numK, numJ))) {
-        console.log(Math.max(numK, numJ) - Math.min(numK, numJ))
-        if (Math.max(numK, numJ) - Math.min(numK, numJ) < result) {
-          result = (Math.max(numK, numJ) - Math.min(numK, numJ))
+      let difference = Math.max(numK, numJ) - Math.min(numK, numJ)
+      if (arr.includes(difference)) {
+        console.log(difference)
+        if (difference < result) {
+          result = difference
         }
       }
     }
