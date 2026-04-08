5ff60fca980c76369e29f5f8a8f5fdf2d0642a57
mrMarsik <petroimoroz@gmail.com>
2026-04-08 06:17:01 +0200

    56

--- 051-100/056/index.js
+++ 051-100/056/index.js
@@ -1,1 +1,19 @@
-console.log(1)
+let maxSum = 0
+
+for (let a = 1n; a < 100n; a++) {
+  for (let b = 1n; b < 100n; b++) {
+    let num = a ** b
+    let arrNum = num.toString().slice('')
+
+    let sum = 0
+    for (const digit of arrNum) {
+      sum += +digit
+    }
+
+    if (sum > maxSum) {
+      maxSum = sum
+    }
+  }
+}
+
+console.log(maxSum)
